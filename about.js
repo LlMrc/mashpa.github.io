54503b7767bdbdcb88fa446de8452c491a97509d
Louis Marc Leonard <llouismrc2@gmail.com>
2023-04-14 06:42:48 -0400

    Add files via upload
    put about section as stand alone page

--- about.js
+++ about.js
@@ -1,12 +1,12 @@
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-     smooth: true,
-     smartphone: {smooth: true},
-     tablet: {smooth: true}
-  });
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+  smartphone: { smooth: true },
+  tablet: { smooth: true },
+});
 
-  const backbtn = document.querySelector('.container-btn')
+const backbtn = document.querySelector(".container-btn");
 
-  backbtn.addEventListener('click', ()=>{
-   window.history.back()
-  })
+backbtn.addEventListener("click", () => {
+  window.history.back();
+});
--- app.js
+++ app.js
@@ -1,42 +1,32 @@
-
 //================================== Lomotive scroll====================
 const scroll = new LocomotiveScroll({
-  el: document.querySelector('[data-scroll-container]'),
-   smooth: true,
-   smartphone: {smooth: true},
-   tablet: {smooth: true}
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+  smartphone: { smooth: true },
+  tablet: { smooth: true },
 });
-
 
 //      ******************************************************************      //
 
 // body color
-scroll.on('scroll', ()=>{
-  if(document.querySelector('#color-bg.is-inview')){
-      document.body.style.background = '#163046'
-      document.body.style.color = '#fefeff'
-  }else{
-    document.body.style.background = '#fefeff'
-    document.body.style.color = '#163046'
+scroll.on("scroll", () => {
+  if (document.querySelector("#color-bg.is-inview")) {
+    document.body.style.background = "#163046";
+    document.body.style.color = "#fefeff";
+  } else {
+    document.body.style.background = "#fefeff";
+    document.body.style.color = "#163046";
   }
-})
+});
 
-const menubtn = document.querySelector('.hamburger-menu')
-const navbtn = document.querySelector('.nav-section')
+const menubtn = document.querySelector(".hamburger-menu");
+const navbtn = document.querySelector(".nav-section");
 
-menubtn.addEventListener('click', ()=>{
-       navbtn.classList.toggle('drawer')
+menubtn.addEventListener("click", () => {
+  navbtn.classList.toggle("drawer");
+});
 
-})
-
-
-const about_btn = document.querySelector('.aboutbt-container')
- about_btn.addEventListener('click', ()=>{
-  window.location.href = "about.html"
- }
- )
-
-
-
-
- 
+const about_btn = document.querySelector(".aboutbt-container");
+about_btn.addEventListener("click", () => {
+  window.location.href = "about.html";
+});
